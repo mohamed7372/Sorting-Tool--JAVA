c4c96f6d800df2a217df1e26aadf93ebb7b0c9fb
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-10-06 12:59:29 +0200

    update5
    we add sorting type by count or natural

--- src/sortingTool/Main.java
+++ src/sortingTool/Main.java
@@ -1,121 +1,162 @@
 package sortingTool;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 	final static Scanner sc = new Scanner(System.in);
 	static ArrayList<String> arr = new ArrayList<String>();
+	static HashMap<String, String> values = new HashMap<String, String>(); 
 	
 	public static void main(String[] args) {
-		if(args.length>2) { 
-			for (int j = 0; j < args.length; j++) { 
-				if(args[j].equals("-sortIntegers")) { 
-					sortIntegers();
-					break;
-				}
-			}	
+		int i = -1, j = -1;
+		if(args.length > 3){
+			if(args[0].equals("-dataType"))
+				i = 0;
+			else if(args[2].equals("-dataType"))
+				i = 2;
+			if(args[0].equals("-sortingType"))
+				j = 0;
+			else if(args[2].equals("-sortingType"))
+				j = 2;
 		}
-		else if(args[0].equals("-sortIntegers"))
-			sortIntegers();
-		else if(args[0].equals("-dataType")) {
-			switch (args[1]) {
+		else
+			i = 0;
+		if(i != -1) {
+			switch (args[i+1]) {
 			case "word":
-				wordData();
+				if(j != -1)
+					wordData(args[j+1]);
+				else 
+					wordData("natural");
 				break;
 			case "line":
-				lineData();
+				if(j != -1)
+					lineData(args[j+1]);
+				else 
+					lineData("natural");
 				break;
 			case "long":
-				longData();
+				if(j != -1)
+					longData(args[j+1]);
+				else 
+					longData("natural");
 				break;
 			}
 		}
 	}
 	
-	static void wordData() {
+	static void wordData(String sortingType) {
 		while(sc.hasNext()) {
 			arr.add(sc.next().trim());
         }
-		int max = 0;
-		for (int i = 1; i < arr.size(); i++) {
-			if(arr.get(i).length() > arr.get(max).length()) 
-				max = i;
-			else if(arr.get(i).length() == arr.get(max).length()) {
-				if(arr.get(i).compareTo(arr.get(max)) < 0)
-					max = i;
-			}
-		}
-		int occ = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			if(arr.get(i).equals(arr.get(max)))
-				occ++;
-		}
 		System.out.printf("Total words: %d.\n", arr.size());
-		System.out.println("The longest word: " + arr.get(max) + " (" + occ + " time(s), " + ((occ*100)/arr.size()) + "%).");
+		if(sortingType.equals("byCount"))
+			byCount("string");
+		else if(sortingType.equals("natural"))
+			natural("word");
 	}
-	static void lineData() {
+	static void lineData(String sortingType) {
 		while(sc.hasNext()) {
 			arr.add(sc.nextLine().trim());
         }
-		int max = 0;
-		for (int i = 1; i < arr.size(); i++) {
-			if(arr.get(i).length() > arr.get(max).length()) 
-				max = i;
-			else if(arr.get(i).length() == arr.get(max).length()) {
-				if(arr.get(i).compareTo(arr.get(max)) < 0)
-					max = i;
-			}
-		}
-		int occ = 0;
-		for (int i = 0; i < arr.size(); i++) {
-			if(arr.get(i).equals(arr.get(max)))
-				occ++;
-		}
-		System.out.printf("Total words: %d.\n", arr.size());
-		System.out.println("The longest line:\n" + arr.get(max) + "\n(" + occ + " time(s), " + ((occ*100)/arr.size()) + "%).");
+		System.out.printf("Total lines: %d.\n", arr.size());
+		if(sortingType.equals("byCount"))
+			byCount("string");
+		else if(sortingType.equals("natural"))
+			natural("line");
 	}
-	static void longData() {
+	static void longData(String sortingType) {
 		while(sc.hasNext()) {
 			arr.add(sc.next().trim());
         }
-		int max = 0, occ = 1;
-		for (int i = 0; i < arr.size(); i++) {
-			if(Integer.valueOf(arr.get(i)) > Integer.valueOf(arr.get(max))) {
-				max = i;
-				occ = 1;
-			}
-			else if(Integer.valueOf(arr.get(i)) == Integer.valueOf(arr.get(max)))
-				occ++;
-		}
 		System.out.printf("Total numbers: %d.\n", arr.size());
-		System.out.println("The largest number: " + arr.get(max) + " (" + occ + " time(s), " + ((occ*100)/arr.size()) + "%).");
+		if(sortingType.equals("byCount"))
+			byCount("long");
+		else if(sortingType.equals("natural"))
+			natural("long");
 	}
 
-	static void sortIntegers() {
-		while(sc.hasNext()) {
-			arr.add(sc.next().trim());
-        }
-		System.out.printf("Total numbers: %d.\n", arr.size());
-		System.out.print("Sorted data: ");
-		sortArr();
-	}
-	static void sortArr() {
-		int[] arrSort = new int[arr.size()];
-		for (int i = 0; i < arrSort.length; i++) {
-			arrSort[i] = Integer.valueOf(arr.get(i));
-		}
-		for (int i = 0; i < arrSort.length - 1; i++) {
-			for (int j = i + 1; j < arrSort.length; j++) {
-				if(arrSort[i] > arrSort[j]) {
-					int c = arrSort[i];
-					arrSort[i] = arrSort[j];
-					arrSort[j] = c;
-				}
+	static void byCount(String type) {
+		for (int i = 0; i < arr.size(); i++) {
+			if(!values.containsKey(arr.get(i))) {
+				int occ = 0;
+				for (String string : arr)
+					occ = arr.get(i).equals(string) ? occ + 1 : occ + 0;
+				values.put(arr.get(i), String.valueOf(occ));
 			}
 		}
-		for (int i = 0; i < arrSort.length-1; i++) 
-			System.out.print(arrSort[i] + " ");
-		System.out.println(arrSort[arrSort.length-1]);
+		if(type.equals("string")) {
+			ArrayList<String> keys = new ArrayList<String>();
+			for (String name : values.keySet()) 
+				keys.add(name);	
+			// sort donne
+			Collections.sort(keys);
+			String[] tab = new String[keys.size()];
+			for (int i = 0; i < tab.length; i++) 
+				tab[i] = keys.get(i);
+			for (int i = 0; i < tab.length-1; i++) {
+				for (int j = i+1; j < tab.length; j++) {
+					if(Integer.valueOf(values.get(tab[i])) > Integer.valueOf(values.get(tab[j]))) {
+						String c = tab[i];
+						tab[i] = tab[j];
+						tab[j] = c;
+					}
+				}
+			}
+			for (String i : tab) { 
+				double d = (Double.valueOf(values.get(i))*100.0)/arr.size();
+				double res = Math.round(d);
+			    System.out.println(i + ": " + values.get(i) + " time(s), " + (int)res + "%).");
+			}
+		}
+		else {
+			ArrayList<Integer> keys = new ArrayList<Integer>();
+			for (String name : values.keySet()) 
+				keys.add(Integer.valueOf(name));
+			// sort donne
+			Collections.sort(keys);
+			int[] tab = new int[keys.size()];
+			for (int i = 0; i < tab.length; i++) 
+				tab[i] = keys.get(i);
+			for (int i = 0; i < tab.length-1; i++) {
+				for (int j = i+1; j < tab.length; j++) {
+					if(Integer.valueOf(values.get(tab[i]+"")) > Integer.valueOf(values.get(tab[j]+""))) {
+						int c = tab[i];
+						tab[i] = tab[j];
+						tab[j] = c;
+					}
+				}
+			}
+			for (int i : tab) { 
+				double d = (Double.valueOf(values.get(i+""))*100.0)/arr.size();
+				double res = Math.round(d);
+			    System.out.println(i + ": " + values.get(i+"") + " time(s), " + (int)res + "%).");
+			}
+		}
+	}
+	static void natural(String typeData) {
+		System.out.print("Sorted data: ");
+		if(typeData.equals("long")) {
+			ArrayList<Integer> ar = new ArrayList<Integer>();
+			for (int i = 0; i < arr.size(); i++) 
+				ar.add(Integer.valueOf(arr.get(i)));
+			Collections.sort(ar);
+			for (Integer integer : ar) 
+				System.out.print(integer + " ");
+		}
+		else if(typeData.equals("line")){
+			System.out.println();
+			Collections.sort(arr);
+			for (String str : arr) 
+				System.out.println(str);
+		}
+		else {
+			Collections.sort(arr);
+			for (String str : arr) 
+				System.out.print(str + " ");
+		}
 	}
 }
